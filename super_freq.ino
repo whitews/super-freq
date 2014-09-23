7e55455ea2167e0c6ca69820a78898633187809d
Scott White <whitews@gmail.com>
2014-09-22 20:58:58 -0400

    added debug mode and better comments, changed a bunch of stuff

--- super_freq.ino
+++ super_freq.ino
@@ -1,5 +1,6 @@
 #define LIN_OUT 1 // use the log output function
 #define FFT_N 128 // set number of FFT points
+#define DEBUG 1   // set to 1 to turn on Serial printing
 
 #include <math.h>
 #include <FFT.h>
@@ -11,9 +12,11 @@ int max_value;
 int sum_fft;
 float frequency;
 float SAMPLE_RATE = 9600.0;
-int SKIP_MULT = 16;
+int SKIP_MULT = 4;
 int MAX_FFT_BIN = 16383;
-int brightness_mult;
+int brightness;  // controls the LED brightness based on dB level
+// LEDs can be set up to 127, but it seems too bright so we tone it down a bit
+int MAX_BRIGHTNESS = 95;
 
 float base_freak = SAMPLE_RATE / FFT_N / 2;
 
@@ -30,7 +33,9 @@ int clockPin = 3;
 LPD8806 strip = LPD8806(nLEDs, dataPin, clockPin);
 
 void setup() {
-    Serial.begin(9600);
+    if (DEBUG) {
+        Serial.begin(9600);
+    }
     TIMSK0 = 0;           // turn off timer0 for lower jitter
     ADCSRA = 0xe5;        // set the adc to free running mode 
     ADMUX = 0x40;         // use adc0
@@ -46,18 +51,25 @@ void setup() {
 void calculateFFT() {
     for (int i = 0 ; i < FFT_N * 2; i += 2) { // save samples
         /* 
-         * sample at slower rate, minimum sample rate is 9.6 kHz
-         * so reduce sample rate by skipping ADC read cycles by 
-         * SKIP_MULT
+         * A hack to sample at a slower rate. 
+         * The minimum sample rate is 9.6 kHz, so reduce sample rate 
+         * by skipping ADC read cycles, essentially spreading out our
+         * sample over time. Reducing the sample rate increases our
+         * frequency resolution, so we can differentiate frequencies 
+         * close to one another, but at the expense of not detecting higher 
+         * frequencies.
+         * 
+         * The effective sample rate would then be:
+         * SAMPLE_RATE / SKIP_MULT
          */
         for (int j = 0; j < SKIP_MULT; j++) {
             while(!(ADCSRA & 0x10)); // wait for adc to be ready
             ADCSRA = 0xf5;           // restart adc
         }
 
-        byte m = ADCL;             // fetch adc data
-        byte j = ADCH;
-        int k = (j << 8) | m;      // form into an int
+        byte m = ADCL;             // fetch ADC data (low byte)
+        byte j = ADCH;             // fetch ADC data (high byte)
+        int k = (j << 8) | m;      // combine low/high bytes to form into an int
         k -= 0x0200;               // form into a signed int
         k <<= 6;                   // form into a 16b signed int
         fft_input[i] = k;          // put real data into even bins
@@ -71,18 +83,16 @@ void calculateFFT() {
 
 // Fill the dots progressively along the strip.
 void setColor(uint32_t c) {
-    int i;
-
-    for (i=0; i < strip.numPixels(); i++) {
+    for (int i=0; i < strip.numPixels(); i++) {
         strip.setPixelColor(i, c);
         strip.show();
     }
 }
 
 void loop() {
-    //cli();  
+    cli();           // disable global interrupts
     calculateFFT();  // result is placed in fft_log_out
-    //sei();
+    sei();           // re-enable interrupts
 
     peak_index = 0;
     max_value = 0;
@@ -92,54 +102,63 @@ void loop() {
         if (max_value < fft_lin_out[i]) {
             // peak index determines frequency
             peak_index = i;
+            // max value determines brightness
             max_value = fft_lin_out[i];
         }
         if (i > 0) {
-            // sum of all bins determines brightness
+            /* 
+             * maybe an alternate way to control brightness
+             * would be the sum of all non-zero bins, i.e. 
+             * the total power in the sample. However, I don't
+             * have a handle on what the maximum sum would be.
+             */
             sum_fft += fft_lin_out[i];
         }
     }
 
     frequency = (peak_index * (SAMPLE_RATE / SKIP_MULT)) / (FFT_N / 4);
-    brightness_mult = floor(95 * max_value / MAX_FFT_BIN);
+    brightness = round(1.0 * MAX_BRIGHTNESS * sum_fft / MAX_FFT_BIN);
     
-    if (max_value < 80 || sum_fft < 400 || peak_index == 0) {
+    if (max_value < 80 || sum_fft < 3000 || peak_index == 0) {
         // signal too weak or we got the 1st peak (0Hz), no lights
         setColor(strip.Color(0, 0, 0));
     } else if (frequency > 0 && frequency < base_freak * 2) {
         // red
-        setColor(strip.Color(brightness_mult,   0,   0));
+        setColor(strip.Color(brightness, 0, 0));
     }
     else if (frequency >= base_freak * 2 && frequency < (base_freak * 3)) {
         // yellow
-        setColor(strip.Color( brightness_mult, brightness_mult,   0));
+        setColor(strip.Color(brightness, brightness, 0));
     }
     else if (frequency >= base_freak * 3 && frequency < (base_freak * 4)) {
         // green
-        setColor(strip.Color(  0, brightness_mult,   0));
+        setColor(strip.Color(0, brightness, 0));
     }
     else if (frequency >= base_freak * 4 && frequency < base_freak * 5) {
         // cyan
-        setColor(strip.Color(  0, brightness_mult, brightness_mult));
+        setColor(strip.Color(0, brightness, brightness));
     }
     else if (frequency >= base_freak * 5 && frequency < base_freak * 6) {
         // blue
-        setColor(strip.Color(  0,   0, brightness_mult));
+        setColor(strip.Color(0, 0, brightness));
     }
     else if (frequency >= base_freak * 6 && frequency < SAMPLE_RATE/4) {
         // violence!!!
-        setColor(strip.Color(brightness_mult,   0, brightness_mult));
+        setColor(strip.Color(brightness, 0, brightness));
     }
     
-    Serial.print("FFT sum: ");
-    Serial.print(sum_fft);
-    Serial.print("\tPeak index: ");
-    Serial.print(peak_index);
-    Serial.print("\tMax value: ");
-    Serial.print(max_value);
-    Serial.print("\tFrequency: ");
-    Serial.println(frequency);
-//    delay(30);
+    if (DEBUG) {
+        Serial.print("FFT sum: ");
+        Serial.print(sum_fft);
+        Serial.print("\tPeak index: ");
+        Serial.print(peak_index);
+        Serial.print("\tMax value: ");
+        Serial.print(max_value);
+        Serial.print("\tFrequency: ");
+        Serial.print(frequency);
+        Serial.print("\tBrightness: ");
+        Serial.println(brightness);
+    }
 }
 
 
